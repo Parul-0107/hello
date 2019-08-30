3c79ec1cdb4c6418ecca16dd387208e3581b5d96
Parul-0107 <54691369+Parul-0107@users.noreply.github.com>
2019-08-29 19:28:42 -0500

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Parul Aggarwal, Halle Starns, Claire Sorenson and Sara Qudus
+ * Date: 2019/08/29
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Parul Aggarwal, Halle Starns, Claire Sorenson and Sara Qudus!\n");
+  printf("Computer Engineering\n");
+  
+  return 0;
+}
